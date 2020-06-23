addd196f22540426025d935fe6abc7270e696ebb
Loïc ARTUSO <artusol@hotmail.fr>
2020-06-23 12:40:54 +0200

    Tp Variables

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,22 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte a= 126;
+		short b= 32766;
+		int c= 1000000;
+		long d=600000000L;
+		float e=3.14f;
+		double f=3145255.5252;
+		char g='b';
+		boolean h=false;
+		String str= "Bonjour";			
+		
+		System.out.println(c);
+		
+	}
+
+}
